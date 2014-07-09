9b3d80d5f406d6a81f7a92928264aeb1ee94a28c
Jiri Stefek <jstefek@redhat.com>
2014-07-09 08:59:58 +0200

    added configuration for chrome, reverted changes in getting Selenium version in startNode.sh

--- startNode.sh
+++ startNode.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 # Downloads selenium server standalone jar and starts selenium grid node with parameters specified in nodeConfig.json
 # can be used with params for the node, like: "sh startNode.sh -Dwebdriver.ie.driver='path-x' ..." 
+# sh startNode.sh -Dwebdriver.ie.driver=IEDriverServer.exe -Dwebdriver.chrome.driver=chromedriver.exe
 
 getSeleniumServer(){
-    	LONG_VERSION=$1
-    	if [ ! -n "$1" ]; then
-        	echo "exiting, no version specified"
-        	exit -1;
-    	fi
-
-    	echo "selenium long version=${LONG_VERSION}"
-	# this could only work when the last version specifier is only 1 character long (e.g. 2.42.2, but not e.g. 2.42.10)
-	SHORT_VERSION=${LONG_VERSION:0:-2}
-    	echo "selenium short version=${SHORT_VERSION}" 
+    SHORT_VERSION=$1
+    if [ ! -n "$1" ]; then
+        echo "exiting, no version specified"
+        exit -1;
+    fi
+    echo "selenium short version=${SHORT_VERSION}"
+    echo $2
+    if [ -z "$2" ]; then
+        LONG_VERSION="${SHORT_VERSION}.0"
+    else
+ 	LONG_VERSION=${SHORT_VERSION}.$2
+    fi
+    echo "selenium long version=${LONG_VERSION}"
 
    	wget -c http://selenium-release.storage.googleapis.com/${SHORT_VERSION}/selenium-server-standalone-${LONG_VERSION}.jar
 }
@@ -33,5 +37,5 @@ runNode(){
 }
 
 trap "sh stop.sh node" INT
-getSeleniumServer 2.42.2
+getSeleniumServer 2.42 2
 runNode "$@"
